93fb73b0ac0dc3b7598eb53e2589946e3f8e79cf
chenlee <chenlee@student.42kl.edu.my>
2022-11-11 15:06:46 +0800

    done i think..

--- solution/selection_sort/selection.c
+++ solution/selection_sort/selection.c
@@ -6,7 +6,7 @@
 /*   By: chenlee <chenlee@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 16:17:36 by chenlee           #+#    #+#             */
-/*   Updated: 2022/11/07 21:57:31 by chenlee          ###   ########.fr       */
+/*   Updated: 2022/11/11 14:43:26 by chenlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,11 @@ void	continue_selection(t_stacks *stacks, int size, int condition)
 // fourth if : for group of any other numbers
 void	selection_sort(t_stacks *stacks, int front, int rear)
 {
+	if (stacks->b_rotate == 1)
+	{
+		rotate(stacks, 'b');
+		stacks->b_rotate--;
+	}
 	if (check_edge_case(stacks, rear - front + 1) != 2)
 		extra(stacks, rear - front, check_edge_case(stacks, rear - front + 1));
 	else if (rear - front >= 5 && rear - front <= 7)
